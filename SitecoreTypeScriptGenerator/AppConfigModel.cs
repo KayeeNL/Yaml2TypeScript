b3731866a226a4266339f25883845b2e1a254961
Dave <david.peet@velir.com>
2024-01-29 14:56:25 -0500

    Move from App.config to appsettings.json file

---
+++ SitecoreTypeScriptGenerator/AppConfigModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SitecoreTypeScriptGenerator
+{
+    public class AppConfigModel
+    {
+        public string? GeneratedFilesOutputPath { get; set; }
+        public string?[]? YamlIncludePathsPiped { get; set; }
+        public string? BaseItemClassName { get; set; }
+        public string? BaseItemClassImportRelativePath { get; set; }
+
+
+        public string? ApplicationWorkingDirectory { get; set; }
+    }
+}
--- SitecoreTypeScriptGenerator/ConfigLoader.cs
+++ SitecoreTypeScriptGenerator/ConfigLoader.cs
@@ -1,29 +1,81 @@
-﻿using System.Configuration;
+﻿
+
+
+using Microsoft.Extensions.Configuration;
 
 namespace SitecoreTypeScriptGenerator
 {
     internal class ConfigLoader
     {
-        public static string? GetRootGenerationPath()
+        internal static IConfiguration Config { get; private set; }
+        private static AppConfigModel Model;
+
+        static ConfigLoader()
         {
-            return ConfigurationManager.AppSettings["GeneratedFilesOutputPath"];
+            Config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+            .Build();
+
+            string workingDirectory = System.Environment.CurrentDirectory;
+            string generatedFilesOutputPath = Config["GeneratedFilesOutputPath"];
+            if(generatedFilesOutputPath != null &&  generatedFilesOutputPath.StartsWith(".\\"))
+            {
+                generatedFilesOutputPath = Path.Combine(workingDirectory, generatedFilesOutputPath.Substring(2));
+            }
+
+            Model = new AppConfigModel
+            {
+                ApplicationWorkingDirectory = System.Environment.CurrentDirectory,
+                GeneratedFilesOutputPath = generatedFilesOutputPath,
+                YamlIncludePathsPiped = GetYamlLoadPaths(workingDirectory, Config.GetSection("YamlIncludePathsPiped")),
+                BaseItemClassName = Config["BaseItemClassName"],
+                BaseItemClassImportRelativePath = Config["BaseItemClassImportRelativePath"]
+            };
         }
 
-        public static string[] GetYamlLoadPaths()
+        public static string? GetRootGenerationPath()
         {
-            var rawValue = ConfigurationManager.AppSettings["YamlIncludePathsPiped"] ?? string.Empty;
-            var entries = rawValue.Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Trim());
-            return entries.ToArray();
+            return Model.GeneratedFilesOutputPath;
+        }
+
+        private static string?[]? GetYamlLoadPaths(string workingDirectory, IConfigurationSection configSection)
+        {
+            var paths = configSection?.GetChildren()?.Select(x => x.Value)?.ToArray() ?? new string?[0];
+            
+            if(!paths.Any()) 
+            {
+                return paths;
+            }
+
+            List<string> finalList = new List<string>();
+            foreach(string p in paths)
+            {
+                if(p!= null && p.StartsWith(".\\"))
+                {
+                    finalList.Add(Path.Combine(workingDirectory, p.Substring(2)));
+                }
+                else
+                {
+                    finalList.Add(p);
+                }
+            }
+
+            return finalList.ToArray();
+        }
+
+        public static string?[] GetYamlLoadPaths()
+        {
+            return Model.YamlIncludePathsPiped ?? new string?[0];
         }
 
         public static string GetBaseItemClassName()
         {
-            return ConfigurationManager.AppSettings["BaseItemClassName"] ?? "Item";
+            return Model?.BaseItemClassName ?? "Item";
         }
 
         public static string GetBaseItemClassImportRelativePath()
         {
-            return ConfigurationManager.AppSettings["BaseItemClassImportRelativePath"] ?? "noPathSet/Missing.ts";
+            return Model.BaseItemClassImportRelativePath ?? "noPathSet/Missing.ts";
         }
     }
 }
